e99233c45154da4ab8acd1f154eded9efa77d184
Melissa Grant-Peters <melissagrantpeters@gmail.com>
2023-03-28 17:34:18 +0100

    Added EWCE for psychiatric genes

--- 04b_EWCE_cell_specificity.R
+++ 04b_EWCE_cell_specificity.R
@@ -7,12 +7,12 @@ set.seed(1234)
 setwd("/Users/melis/Documents/GitHub/LR_project")
 
 load("C:/Users/melis/Downloads/ctd_aibsMultipleCrtxSmrtSeq.rda")  #Load CTD data
-n = 100000
+n = 10000
 
 
 #################------------------- 0.7 threshold
-data = read.csv("processed_data/03-LR_network_visualisation/louvain_largest_cluster_0.7.csv", row.names = 1)
-list = data$X0
+data = read.csv("processed_data/psychiatric_gene_clusters.csv")
+list = data$symbol
 #Load target gene list
 
 full_results <- EWCE::bootstrap_enrichment_test(sct_data = ctd,
@@ -24,18 +24,18 @@ full_results <- EWCE::bootstrap_enrichment_test(sct_data = ctd,
                                                 geneSizeControl = TRUE)         #Control for GC content and gene length
 thisResult1 = data.frame(full_results$results)
 thisResult1$MajorCellType = unlist(lapply(strsplit(as.character(thisResult1$CellType),'_'), `[[`, 1))
-thisResult1$testList = '07_threshold'
+thisResult1$testList = 'psyCluster'
 FinalResult1 = thisResult1
 
 
 plot_list = EWCE::ewce_plot(total_res = full_results$results,                     #Write plot data with BH correction p vals
-                            mtc_method = "FDR",
+                            mtc_method = "BH",
                             ctd = ctd)
 
 plot_list$withDendro+ theme(text = element_text(size = 12))+theme(plot.margin=grid::unit(c(0.5,0.5,0.5,0.5), "in"))   
 
 ggsave(
-  filename="plots/03-LR_network_visualisation/03c-specificity_major_network/annotation1_thr07.png",
+  filename="plots/04-psychiatric_specificity_GTEx/psyCluster_EWCE_ann1_aibs.pdf",
   plot = last_plot(),
   device ="png",
   scale = 1,
@@ -45,7 +45,7 @@ ggsave(
   dpi = 300
 )
 
-write.csv(data.frame(full_results$results), "processed_data/03-LR_network_visualisation/03c-specificity_major_network/annotation1_thr07.csv", row.names=TRUE, quote=FALSE) 
+write.csv(data.frame(full_results$results), "processed_data/04-psychiatric_specificity_GTEx/psyCluster_EWCE_ann1_aibs.csv", row.names=TRUE, quote=FALSE) 
 
 rm(full_results)
 rm(plot_list)
@@ -60,7 +60,7 @@ full_results <- EWCE::bootstrap_enrichment_test(sct_data = ctd,
 
 thisResult = data.frame(full_results$results)
 thisResult$MajorCellType = unlist(lapply(strsplit(as.character(thisResult$CellType),'_'), `[[`, 1))
-thisResult$testList = '07_threshold'
+thisResult$testList = 'psyCluster'
 FinalResult = thisResult
 
 plot_list = EWCE::ewce_plot(total_res = full_results$results,                     #Write plot data with BH correction p vals
@@ -70,7 +70,7 @@ plot_list = EWCE::ewce_plot(total_res = full_results$results,                   
 plot_list$withDendro+ theme(text = element_text(size = 12))+theme(plot.margin=grid::unit(c(0.5,0.5,0.5,0.5), "in"))   
 
 ggsave(
-  filename="plots/03-LR_network_visualisation/03c-specificity_major_network/annotation2_thr07.png",
+  filename="plots/04-psychiatric_specificity_GTEx/psyCluster_EWCE_ann2_aibs.pdf",
   plot = last_plot(),
   device ="png",
   scale = 1,
@@ -80,163 +80,9 @@ ggsave(
   dpi = 300
 )
 
-write.csv(data.frame(full_results$results), "processed_data/03-LR_network_visualisation/03c-specificity_major_network/annotation2_thr07.csv", row.names=TRUE, quote=FALSE) 
+write.csv(data.frame(full_results$results), "processed_data/04-psychiatric_specificity_GTEx/psyCluster_EWCE_ann2_aibs.csv", row.names=TRUE, quote=FALSE) 
 rm(full_results)
 rm(plot_list)
-
-#################------------------- Threshold 0.4
-#Load target gene list
-data = read.csv("processed_data/03-LR_network_visualisation/louvain_largest_cluster_0.4.csv", row.names = 1)
-list = data$X0
-full_results <- EWCE::bootstrap_enrichment_test(sct_data = ctd,
-                                                sctSpecies = "human",
-                                                genelistSpecies = "human",
-                                                hits = list, 
-                                                reps = n,                       #Bootstrap repeats set to 10000
-                                                annotLevel = 1,                 #Annotation level where 1= major cell types, 2= higher resolution
-                                                geneSizeControl = TRUE)         #Control for GC content and gene length
-
-thisResult1 = data.frame(full_results$results)
-thisResult1$MajorCellType = unlist(lapply(strsplit(as.character(thisResult1$CellType),'_'), `[[`, 1))
-thisResult1$testList = '04_threshold'
-FinalResult1 = rbind(FinalResult1,thisResult1)
-
-plot_list = EWCE::ewce_plot(total_res = full_results$results,                     #Write plot data with BH correction p vals
-                            mtc_method = "FDR",
-                            ctd = ctd)
-
-plot_list$withDendro+ theme(text = element_text(size = 12))+theme(plot.margin=grid::unit(c(0.5,0.5,0.5,0.5), "in"))   
-
-ggsave(
-  filename="plots/03-LR_network_visualisation/03c-specificity_major_network/annotation1_thr04.png",
-  plot = last_plot(),
-  device ="png",
-  scale = 1,
-  width = 7,
-  height = 7,
-  units = c("in"),
-  dpi = 300
-)
-
-write.csv(data.frame(full_results$results), "processed_data/03-LR_network_visualisation/03c-specificity_major_network/annotation1_thr04.csv", row.names=TRUE, quote=FALSE) 
-
-rm(full_results)
-rm(plot_list)
-
-full_results <- EWCE::bootstrap_enrichment_test(sct_data = ctd,
-                                                sctSpecies = "human",
-                                                genelistSpecies = "human",
-                                                hits = list, 
-                                                reps = n,                       #Bootstrap repeats set to 10000
-                                                annotLevel = 2,                 #Annotation level where 1= major cell types, 2= higher resolution
-                                                geneSizeControl = TRUE)         #Control for GC content and gene length
-thisResult = data.frame(full_results$results)
-thisResult$MajorCellType = unlist(lapply(strsplit(as.character(thisResult$CellType),'_'), `[[`, 1))
-thisResult$testList = '04_threshold'
-FinalResult = rbind(FinalResult,thisResult)
-
-plot_list = EWCE::ewce_plot(total_res = full_results$results,                     #Write plot data with BH correction p vals
-                            mtc_method = "FDR",
-                            ctd = ctd)
-
-plot_list$withDendro+ theme(text = element_text(size = 12))+theme(plot.margin=grid::unit(c(0.5,0.5,0.5,0.5), "in"))   
-
-ggsave(
-  filename="plots/03-LR_network_visualisation/03c-specificity_major_network/annotation2_thr04.png",
-  plot = last_plot(),
-  device ="png",
-  scale = 1,
-  width = 20,
-  height = 7,
-  units = c("in"),
-  dpi = 300
-)
-
-write.csv(data.frame(full_results$results), "processed_data/03-LR_network_visualisation/03c-specificity_major_network/annotation2_thr04.csv", row.names=TRUE, quote=FALSE) 
-rm(full_results)
-rm(plot_list)
-
-#################------------------- Threshold 0.1
-#Load target gene list
-
-list = data[data$Splicing.regulation>0,1]
-
-full_results <- EWCE::bootstrap_enrichment_test(sct_data = ctd,
-                                                sctSpecies = "human",
-                                                genelistSpecies = "human",
-                                                hits = list, 
-                                                reps = n,                       #Bootstrap repeats set to 10000
-                                                annotLevel = 1,                 #Annotation level where 1= major cell types, 2= higher resolution
-                                                geneSizeControl = TRUE)         #Control for GC content and gene length
-
-thisResult1 = data.frame(full_results$results)
-thisResult1$MajorCellType = unlist(lapply(strsplit(as.character(thisResult1$CellType),'_'), `[[`, 1))
-thisResult1$testList = '01_threshold'
-FinalResult1 = rbind(FinalResult1,thisResult1)
-
-plot_list = EWCE::ewce_plot(total_res = full_results$results,                     #Write plot data with BH correction p vals
-                            mtc_method = "FDR",
-                            ctd = ctd)
-
-plot_list$withDendro+ theme(text = element_text(size = 12))+theme(plot.margin=grid::unit(c(0.5,0.5,0.5,0.5), "in"))   
-
-ggsave(
-  filename="plots/03-LR_network_visualisation/03c-specificity_major_network/annotation1_thr01.png",
-  plot = last_plot(),
-  device ="png",
-  scale = 1,
-  width = 7,
-  height = 7,
-  units = c("in"),
-  dpi = 300
-)
-
-write.csv(data.frame(full_results$results), "processed_data/03-LR_network_visualisation/03c-specificity_major_network/annotation1_thr01.csv", row.names=TRUE, quote=FALSE) 
-
-rm(full_results)
-rm(plot_list)
-
-full_results <- EWCE::bootstrap_enrichment_test(sct_data = ctd,
-                                                sctSpecies = "human",
-                                                genelistSpecies = "human",
-                                                hits = list, 
-                                                reps = n,                       #Bootstrap repeats set to 10000
-                                                annotLevel = 2,                 #Annotation level where 1= major cell types, 2= higher resolution
-                                                geneSizeControl = TRUE)         #Control for GC content and gene length
-thisResult = data.frame(full_results$results)
-thisResult$MajorCellType = unlist(lapply(strsplit(as.character(thisResult$CellType),'_'), `[[`, 1))
-thisResult$testList = '01_threshold'
-FinalResult = rbind(FinalResult,thisResult)
-
-plot_list = EWCE::ewce_plot(total_res = full_results$results,                     #Write plot data with BH correction p vals
-                            mtc_method = "FDR",
-                            ctd = ctd)
-
-plot_list$withDendro+ theme(text = element_text(size = 12))+theme(plot.margin=grid::unit(c(0.5,0.5,0.5,0.5), "in"))   
-
-ggsave(
-  filename="plots/03-LR_network_visualisation/03c-specificity_major_network/annotation2_thr01.png",
-  plot = last_plot(),
-  device ="png",
-  scale = 1,
-  width = 20,
-  height = 7,
-  units = c("in"),
-  dpi = 300
-)
-
-write.csv(data.frame(full_results$results), "processed_data/03-LR_network_visualisation/03c-specificity_major_network/annotation2_thr01.csv", row.names=TRUE, quote=FALSE) 
-rm(full_results)
-rm(plot_list)
-
-############-------------------------------- FINAL SAVE and PLOT
-
-write.csv((.packages()), "../processed-data/loaded_R_packages.R")
-
-
-write.csv(FinalResult1, "../processed-data/final_results_table_annot1.csv")
-sig1 = FinalResult1[FinalResult1$q>0.05,]
-
 
 FinalResult1 %>% 
   mutate(CellType = factor(CellType,c("Astrocyte", "Microglia","OPC", "Oligodendrocyte","Vascular_cells","GABAergic_LAMP5","GABAergic_PAX6","GABAergic_PVALB","GABAergic_SST","GABAergic_VIP","Glutamatergic_IT","Glutamatergic_L4_IT","Glutamatergic_L5_ET","Glutamatergic_L5_6_IT_Car3","Glutamatergic_L5_6_NP", "Glutamatergic_L6_CT","Glutamatergic_L6b"))) %>%        
@@ -265,7 +111,7 @@ FinalResult1 %>%
         ## This is to plot the two legends in two rows
         legend.box="vertical") 
 ggsave(
-  filename="plots/03-LR_network_visualisation/03c-specificity_major_network/annotation1_multiple_thr.png",
+  filename="plots/04-psychiatric_specificity_GTEx/annotation1_multiple_thr.png",
   plot = last_plot(),
   device ="png",
   scale = 1,
@@ -277,7 +123,7 @@ ggsave(
 
 sig = FinalResult[FinalResult$p<0.05,]
 
-write.csv(FinalResult, "processed_data/03-LR_network_visualisation/03c-specificity_major_network/annotation1_multiple_thr.csv")
+#write.csv(FinalResult, "processed_data/03-LR_network_visualisation/03c-specificity_major_network/annotation1_multiple_thr.csv")
 
 ggplot(FinalResult) +
   geom_point(aes(x = -log10(q), y = CellType, color = MajorCellType, size = fold_change, alpha = q<0.05))+
@@ -304,7 +150,7 @@ ggplot(FinalResult) +
         ## This is to plot the two legends in two rows
         legend.box="vertical") 
 ggsave(
-  filename="plots/03-LR_network_visualisation/03c-specificity_major_network/annotation2_multiple_thr.png",
+  filename="plots/04-psychiatric_specificity_GTEx/annotation2_multiple_thr.png",
   plot = last_plot(),
   device ="png",
   scale = 1,
