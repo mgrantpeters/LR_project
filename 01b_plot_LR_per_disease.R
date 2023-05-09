751298fac0c612c77d8998c5a40c304536166993
Melissa Grant-Peters <melissagrantpeters@gmail.com>
2023-05-09 11:39:23 +0100

    fixing plots across thresholds

--- 01b_plot_LR_per_disease.R
+++ 01b_plot_LR_per_disease.R
@@ -8,6 +8,7 @@ setwd("/Users/melis/Documents/GitHub/LR_project/")
 data = read.csv("processed_data/LR_occurance.csv")
 
 data %>% 
+  mutate(Disease = factor(Disease,rev(c("AD","ALS","EssentialTremor","FrontotemporalDementia","LBD","PD","ProgressiveSupranuclearPalsy","AnorexiaNervosa","BipolarDisorder","MajorDepressiveDisorder","NeuroticDisorder","OCD","Schizophrenia","TouretteSyndrome","UnipolarDepression","BrainAneurysm","IntracranialHemorrhage","MigraineDisorder","MigraineWithAura","MS","NarcolepsyCataplexy","Narcolepsy","PartialEpilepsy","RestlessLeg")))) %>%
   ggplot() +
   geom_point(aes(x = -log10(pval), y = Disease,color = Disease, size = perc.LR, alpha = pval<0.05), show.legend = FALSE)+
   scale_alpha_discrete(range=c(0.4,1))+
@@ -33,12 +34,94 @@ data %>%
         ## This is to plot the two legends in two rows
         #legend.box="vertical") 
 ggsave(
-  filename="plots/LR_occurance_per_disease.png",
+  filename="plots/LR_occurance_per_disease_01.png",
   plot = last_plot(),
   device ="png",
   scale = 1,
   width = 7,
   height = 8,
+  units = c("in"),
+  dpi = 300
+)
+
+#####################################
+
+data = read.csv("processed_data/LR_occurance_0.4.csv")
+
+data %>% 
+  mutate(Disease = factor(Disease,rev(c("AD","ALS","EssentialTremor","FrontotemporalDementia","LBD","PD","ProgressiveSupranuclearPalsy","AnorexiaNervosa","BipolarDisorder","MajorDepressiveDisorder","NeuroticDisorder","OCD","Schizophrenia","TouretteSyndrome","UnipolarDepression","BrainAneurysm","IntracranialHemorrhage","MigraineDisorder","MigraineWithAura","MS","NarcolepsyCataplexy","Narcolepsy","PartialEpilepsy","RestlessLeg")))) %>%
+  ggplot() +
+  geom_point(aes(x = -log10(pval), y = Disease,color = Disease, size = perc.LR, alpha = pval<0.05), show.legend = FALSE)+
+  scale_alpha_discrete(range=c(0.4,1))+
+  facet_grid(cols = vars(LR))+
+  theme(text = element_text(size = 12))+ theme(plot.margin=grid::unit(c(0.5,0.5,0.5,0.5), "in"))+
+  geom_vline(xintercept=1.3010, linetype="dashed", color = "darkgray") +
+  xlab("-log10(p)") +
+  ylab("Disease") +
+  #coord_flip() + # this is to make  the graph landscape
+  theme_light() +
+  viridis::scale_fill_viridis() + ## to make the colour scheme color-blind safe, the parameter option="A" or option="B",etc will change palette
+  #guides(fill = guide_legend(title = "Disease")) +
+  theme(axis.line = element_line(colour = "black"), 
+        axis.text = element_text(colour = "black", size = "10"),
+        axis.text.x = element_text(colour = "black", size = "10", angle=90, hjust=1),
+        axis.title = element_text(colour = "black", size = "10"),
+        strip.text = element_text(colour = "black", size = "10"), 
+        #legend.text = element_text(colour = "black", size = "10"),
+        plot.caption = element_text(colour = "black", size = "10"),
+        plot.title = element_text(colour = "black", size = "10"))
+#legend.title = element_text(colour = "black", size = "10"),
+#legend.position = "top",
+## This is to plot the two legends in two rows
+#legend.box="vertical") 
+ggsave(
+  filename="plots/LR_occurance_per_disease_04.png",
+  plot = last_plot(),
+  device ="png",
+  scale = 1,
+  width = 7,
+  height = 6,
+  units = c("in"),
+  dpi = 300
+)
+
+####################################################
+
+data = read.csv("processed_data/LR_occurance_0.7.csv")
+
+data %>% 
+  mutate(Disease = factor(Disease,rev(c("AD","ALS","PD","BipolarDisorder","Schizophrenia","UnipolarDepression","MigraineDisorder","PartialEpilepsy")))) %>%
+  ggplot() +
+  geom_point(aes(x = -log10(pval), y = Disease,color = Disease, size = perc.LR, alpha = pval<0.05), show.legend = FALSE)+
+  scale_alpha_discrete(range=c(0.4,1))+
+  facet_grid(cols = vars(LR))+
+  theme(text = element_text(size = 12))+ theme(plot.margin=grid::unit(c(0.5,0.5,0.5,0.5), "in"))+
+  geom_vline(xintercept=1.3010, linetype="dashed", color = "darkgray") +
+  xlab("-log10(p)") +
+  ylab("Disease") +
+  #coord_flip() + # this is to make  the graph landscape
+  theme_light() +
+  viridis::scale_fill_viridis() + ## to make the colour scheme color-blind safe, the parameter option="A" or option="B",etc will change palette
+  #guides(fill = guide_legend(title = "Disease")) +
+  theme(axis.line = element_line(colour = "black"), 
+        axis.text = element_text(colour = "black", size = "10"),
+        axis.text.x = element_text(colour = "black", size = "10", angle=90, hjust=1),
+        axis.title = element_text(colour = "black", size = "10"),
+        strip.text = element_text(colour = "black", size = "10"), 
+        #legend.text = element_text(colour = "black", size = "10"),
+        plot.caption = element_text(colour = "black", size = "10"),
+        plot.title = element_text(colour = "black", size = "10"))
+#legend.title = element_text(colour = "black", size = "10"),
+#legend.position = "top",
+## This is to plot the two legends in two rows
+#legend.box="vertical") 
+ggsave(
+  filename="plots/LR_occurance_per_disease_07.png",
+  plot = last_plot(),
+  device ="png",
+  scale = 1,
+  width = 7,
+  height = 4,
   units = c("in"),
   dpi = 300
 )
