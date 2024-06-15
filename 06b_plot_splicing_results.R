0736191fa0c1b5982aad0184da592868ed8f988e
Melissa Grant-Peters <melissagrantpeters@gmail.com>
2024-06-15 07:50:09 +0100

    publication edits

---
+++ 06b_plot_splicing_results.R
@@ -0,0 +1,57 @@
+library("biomaRt")
+library("ggplot2")
+library("grid")
+library("dplyr")
+set.seed(1234)
+setwd("/Users/melis/Documents/GitHub/LR_project/")
+all_data = read.csv("processed_data/06a_splicing_disease_LRs/significance_splicing_disease.csv")
+all_data = all_data[is.nan(all_data)] = 0.0
+data = filter(all_data, Threshold == 0.40)
+data = filter(data, type != 'LR')
+data$padj = p.adjust(data$pval, method="fdr")
+data$Ligand_padj = p.adjust(data$Ligand.pval, method="fdr")
+data$Receptor_padj = p.adjust(data$Receptor_pval, method="fdr")
+data %>% 
+  mutate(Disease = factor(Disease,rev(c("AD","ALS","EssentialTremor","FrontotemporalDementia","LBD","PD","ProgressiveSupranuclearPalsy","AnorexiaNervosa","BipolarDisorder","MajorDepressiveDisorder","NeuroticDisorder","OCD","Schizophrenia","TouretteSyndrome","UnipolarDepression","BrainAneurysm","IntracranialHemorrhage","MigraineDisorder","MigraineWithAura","MS","NarcolepsyCataplexy","Narcolepsy","PartialEpilepsy","RestlessLeg")))) %>%
+  ggplot() +
+  geom_point(aes(x = -log10(padj), y = Disease,color = Disease, size = Median.transcript.variants, alpha = padj<0.05), show.legend = TRUE)+
+  scale_alpha_discrete(range=c(0.4,1))+
+  facet_grid(cols = vars(type))+
+  theme(text = element_text(size = 12))+ theme(plot.margin=grid::unit(c(0.5,0.5,0.5,0.5), "in"))+
+  geom_vline(xintercept=1.3010, linetype="dashed", color = "darkgray") +
+  xlab("-log10(p)") +
+  ylab("Disease") +
+  theme_light() +
+  viridis::scale_fill_viridis() + ## to make the colour scheme color-blind safe, the parameter option="A" or option="B",etc will change palette
+  theme(axis.line = element_line(colour = "black"), 
+        axis.text = element_text(colour = "black", size = "10"),
+        axis.text.x = element_text(colour = "black", size = "10", angle=90, hjust=1),
+        axis.title = element_text(colour = "black", size = "10"),
+        strip.text = element_text(colour = "black", size = "10"), 
+        legend.text = element_text(colour = "black", size = "10"),
+        plot.caption = element_text(colour = "black", size = "10"),
+        plot.title = element_text(colour = "black", size = "10"),
+        legend.position = "top",
+        legend.box="vertical")
+
+ggsave(
+  filename="plots/06a_splicing_disease_LRs/LR_variants_across_diseases_04.pdf",
+  plot = last_plot(),
+  device ="pdf",
+  scale = 1,
+  width = 6,
+  height = 8,
+  units = c("in"),
+  dpi = 300
+)
+ggsave(
+  filename="plots/06a_splicing_disease_LRs/LR_variants_across_diseases_04.png",
+  plot = last_plot(),
+  device ="png",
+  scale = 1,
+  width = 6,
+  height = 8,
+  units = c("in"),
+  dpi = 300
+)
+
